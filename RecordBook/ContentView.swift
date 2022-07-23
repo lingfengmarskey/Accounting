a1f21bf327801e2be4efcee702af7a7c4994ed9d
MarcosMeng <marcosmang09@gmail.com>
2022-07-23 16:36:11 +0900

    Initial Commit

---
+++ RecordBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecordBook
+//
+//  Created by Marcos Meng on 2022/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecordBook/RecordBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordBookApp.swift
+//  RecordBook
+//
+//  Created by Marcos Meng on 2022/07/23.
+//
+
+import SwiftUI
+
+@main
+struct RecordBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
